1199e0f98f434fdf33fffa9c823e1fbdc855cdaf
omar <esgaib@gmail.com>
2020-10-24 15:07:50 -0300

    ajuste welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,34 +65,22 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div class="top-right links">
+                <ul>
+                    <li>Nombre - Apellido - Matricula aqui</li>
+                </ul>
+            </div>
+
 
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                Enlaces:
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <!-- Agregar los enlaces a cada pagina aqui -->
+
                 </div>
             </div>
         </div>
