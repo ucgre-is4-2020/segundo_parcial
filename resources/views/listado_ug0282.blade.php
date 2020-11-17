770c765559375a6a63e3a74bd88c1cabf54f338d
omar <esgaib@gmail.com>
2020-11-16 23:58:06 -0300

    correccion de nombre de ruta pagina principal

--- resources/views/listado_ug0282.blade.php
+++ resources/views/listado_ug0282.blade.php
@@ -1,7 +1,7 @@
 <style type="text/css">
 #mensaje{
 	color: green;
-}	
+}
 #centro{
 		border:3px solid green;
 		width: 65%;
@@ -24,25 +24,25 @@
 		<a href="{{route('borrar_tubo_estado',['id'=>$unestado->id])}}" onclick="return confirm('Desea borrar el registro?')">Borrar - </a>
 		<a href="{{route('ver_tubo_estado',['id'=>$unestado->id])}}">Ver - </a>
 		<a href="{{route('editar_tubo_estado',['id'=>$unestado->id])}}">Editar - </a>
-		
-		
-		{{$unestado->nombre}} - 
+
+
+		{{$unestado->nombre}} -
 		<?php If ($unestado->activo == "1"){
-				echo "activo - "; 
+				echo "activo - ";
 				}else{
 				echo "inactivo - ";
 				}
 		?>
-		{{$unestado->created_at}} - 
+		{{$unestado->created_at}} -
 		{{$unestado->updated_at}}
-		
+
 		</li>
-		
+
 	@endforeach
 	<input name="buscarpor" type="search" placeholder="Buscar por nombre" aria-label="Search">
     <button type="submit">Buscar</button>
 </ul>
 </form>
-<a href="{{route('raiz')}}">Volver a la página Principal</a><br>
+<a href="{{route('welcome')}}">Volver a la página Principal</a><br>
 <a href="{{route('listado_tubo_estado')}}">Ver Listado Completo</a>
 </div>
