ecb86457b2b794071f202ba1b1b1f04b9bb6e228
omar <esgaib@gmail.com>
2020-11-19 21:45:32 -0300

    se corrige enlace

--- resources/views/listado-ug0287.blade.php
+++ resources/views/listado-ug0287.blade.php
@@ -8,7 +8,7 @@
 
 				    <input name="buscarpor" class="form-control mr-sm-2" type="search" placeholder="Buscar por nombre" aria-label="Search">
 				    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-				   
+
 				  </form>
 				</nav><br>
 
@@ -16,7 +16,7 @@
     @foreach($misFacturas as $unaFactura)
         <li>
             <a href="{{route('editar-ug0287',['id' =>$unaFactura->id])}}">EditarFactura</a>
-            
+
 
             <a href="{{ route ('borrar-ug0287', ['id' => $unaFactura->id]) }}" onclick="return confirm('¿Está seguro de borrar este registro?')">BorrarFactura</a>
 
@@ -28,4 +28,4 @@
 </ul>
 <h5>Tengo en total {{count($misFacturas)}} listado-ug0287</h5>
 
-<a href="{{ route('raiz') }}">Volver al inicio </a>
+<a href="{{ route('welcome') }}">Volver al inicio </a>
